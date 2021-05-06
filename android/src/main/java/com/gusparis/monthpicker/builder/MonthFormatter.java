d534647adecac7556cad669a6eb06c3945c5d545
Mikhail Smirnov <smirnov.m.n.91@gmail.com>
2021-05-06 18:19:45 +0600

    fix android months

--- android/src/main/java/com/gusparis/monthpicker/builder/MonthFormatter.java
+++ android/src/main/java/com/gusparis/monthpicker/builder/MonthFormatter.java
@@ -4,61 +4,73 @@ import android.annotation.SuppressLint;
 import android.widget.NumberPicker;
 
 import java.text.DateFormatSymbols;
+import java.text.ParsePosition;
+import java.text.SimpleDateFormat;
+import java.time.Month;
+import java.time.format.TextStyle;
+import java.util.Locale;
 
 public class MonthFormatter {
 
-  public static NumberPicker.Formatter getMonthFormatter(String type, DateFormatSymbols dfs) {
-    switch (type) {
-      case "short":
-        return new ShortMonth(dfs);
-      case "number":
-        return new NumberMonth();
-      case "shortNumber":
-        return new ShortNumberMonth();
-      default:
-        return new FullMonth(dfs);
-    }
-  }
-
-  private static class FullMonth implements NumberPicker.Formatter {
-    private String [] months;
-
-    public FullMonth(DateFormatSymbols dfs) {
-      months = dfs.getMonths();
+    public static NumberPicker.Formatter getMonthFormatter(String type, DateFormatSymbols dfs, Locale locale) {
+        switch (type) {
+            case "short":
+                return new ShortMonth(dfs);
+            case "number":
+                return new NumberMonth();
+            case "shortNumber":
+                return new ShortNumberMonth();
+            default:
+                return new FullMonth(dfs, locale);
+        }
     }
 
-    @Override
-    public String format(int i) {
-      return months[i];
+    private static class FullMonth implements NumberPicker.Formatter {
+        private String[] months;
+
+        public FullMonth(DateFormatSymbols dfs, Locale locale) {
+            String[] _months = new String[12];
+            SimpleDateFormat monthDisplay = new SimpleDateFormat("LLLL", locale);
+            SimpleDateFormat monthParse = new SimpleDateFormat("MM", locale);
+            for (int i = 0; i < 12; i++) {
+                String month = String.valueOf(i);
+                _months[i] = monthDisplay.format(monthParse.parse(month,new ParsePosition(0)));
+            }
+            months = _months;
+        }
+
+        @Override
+        public String format(int i) {
+            return months[i];
+        }
     }
-  }
 
-  private static class ShortMonth implements NumberPicker.Formatter {
-    private String [] months;
+    private static class ShortMonth implements NumberPicker.Formatter {
+        private String[] months;
 
-    public ShortMonth(DateFormatSymbols dfs) {
-      months = dfs.getShortMonths();
+        public ShortMonth(DateFormatSymbols dfs) {
+            months = dfs.getShortMonths();
+        }
+
+        @Override
+        public String format(int i) {
+            return months[i];
+        }
     }
 
-    @Override
-    public String format(int i) {
-      return months[i];
+    private static class NumberMonth implements NumberPicker.Formatter {
+
+        @Override
+        public String format(int i) {
+            return String.format("%02d", i + 1);
+        }
     }
-  }
 
-  private static class NumberMonth implements NumberPicker.Formatter {
+    private static class ShortNumberMonth implements NumberPicker.Formatter {
 
-    @Override
-    public String format(int i) {
-      return String.format("%02d", i + 1);
+        @Override
+        public String format(int i) {
+            return String.valueOf(i + 1);
+        }
     }
-  }
-
-  private static class ShortNumberMonth implements NumberPicker.Formatter {
-
-    @Override
-    public String format(int i) {
-      return String.valueOf(i + 1);
-    }
-  }
 }
